b8908d8fbfd11453c98c603d7756ef6596c245f5
Nathan Guardado <148016985+NathanG741@users.noreply.github.com>
2024-02-02 05:23:30 -0500

    Time: 4 ms (100.00%), Space: 44.2 MB (27.35%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
+
+    // base case 
     if (x == 0) {
         return true;
     }
+    // if x is negative or x ends with 0
     if (x < 0 || x % 10 == 0) {
         return false;
     }
 
+    //reversing half of the integer & compare
     int reversed_int = 0;
         while(x > reversed_int) {
+    //get and removes the last digit
         int pop = x % 10;
         x /= 10;
 
